3d3ccba41e45a1f27d6fe940f2840e245eeadd26
likin gera <likingera@gmail.com>
2019-02-11 00:25:12 +0000

    changed background color ,code cleanup

--- src/main/resources/static/js/jqajaxscripts.js
+++ src/main/resources/static/js/jqajaxscripts.js
@@ -8,16 +8,16 @@ $(document).ready(function() {
             jsonpCallback: 'courses',
             success: function(result){
        
-                console.debug(result);
+                
 
                 $.each(result.courses, function(i, course) {
-                     console.debug(course);
+                     
                     $("tbody").append("<tr class='coursedata bgclass'><td class='anchortd'><a class='anchortag' href='#'>+</a></td><td>"+ course.id +"</td><td>"+ course.start+"</td><td>"+ course.end+"</td><td>"+ course.qualification+"</td></tr>");
 
                     $("tbody").append("<tr class='studentdata'><td/><td>Student ID</td><td>Name</td></tr>");
                     $.each(course.students, function(j, student) {
-                        console.debug(student);
-                        $("tbody").append("<tr class='studentdata bgclass'><td></td><td>"+ student.id +"</td><td>"+ student.name+"</td></tr>");
+                        
+                        $("tbody").append("<tr class='studentdata stubgclass'><td></td><td>"+ student.id +"</td><td>"+ student.name+"</td></tr>");
 
                     
                     });
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,33 +1,11 @@
 $(document).ready(function() {
 
 
-    $.ajax({
-            url: "http://18.130.253.44/getCourses.php?action=getUnpaid",  
-            
-            dataType: "jsonp",
-            jsonpCallback: 'courses',
-            success: function(result){
-       
-            console.debug("result"+result);
-            }
-
-});
-
-
-
-
- 
  $(".studentdata").hide();
 
     $(".anchortag").click(function() {
 
-
-        
-
-         $(this).text($(this).text()=='+' ? '-':'+');
-
-
-        console.debug($(this).parent('.coursedata').nextUntil('.coursedata'));
+        $(this).text($(this).text()=='+' ? '-':'+');
         $(this).parent('.anchortd').parent('.coursedata').nextUntil('.coursedata').fadeToggle();
     });
 });
